89f901977c8c82376dfa1fdb5e89c0b28c06570d
Henoc Dz <hdz@rfdz.mx>
2013-04-08 21:47:54 -0500

    Subredes sobrantes clase B

--- VLSM.py
+++ VLSM.py
@@ -9,32 +9,51 @@ class VLSM(object):
 		self.ip = ip
 		self.deptos_vlsm = []
 
-	def deptoss(self,deptos):
-		libres = 256 - self.totalHosts()
-		lbs = self.calculaLibres(libres)
+	def add_deptos_libres(self,deptos):
+		tipoIP = (self.ip).getTipo()
+		if tipoIP is 'C':
+			rango = 8
+			bitex = 1
+		elif tipoIP is 'B':
+			rango = 16
+			bitsex = 2
+		elif tipoIP is 'C':
+			rango = 24
+		else:
+			exit(1)
 
+
+		libres = ((self.ip).getHosts() + bitsex) - self.totalHosts()
+
+		lbs = self.calculaLibres(libres,rango)
+
+		deptos_libres = []
+
+		libre_id = range(1,len(lbs)+1)
+		libre_id.reverse()
 		c = 0
-		t = 0
 		for l in lbs:
-			t += l[0]
-			c += 1
-			deptos.append({
-				'nombre': 'Libre ' + str(c),
+			deptos_libres.append({
+				'nombre': 'Libre ' + str(libre_id[c]),
 				'red': '',
 				'broadcast': '',
-				'hosts':l[0],
+				'hosts':l[0] - 2,
 				'bits_host':l[1],
-				'msr': '255.255.255.0'
+				'msr': ''
 			})
+			c += 1
 
-		print t
+		deptos_libres_sort = sorted(deptos_libres, key=lambda k: k['hosts'],reverse = False)
 
-		return sorted(deptos, key=lambda k: k['hosts'],reverse = True)
+		for depto_libre in deptos_libres_sort:
+			deptos.append(depto_libre)
+
+		return deptos
 
 	#Comprueba que se pueda realizar la operacion
 	def validar(self):
 		r = (self.ip).hostsDisponibles(self.totalHosts())
-		self.deptos = self.deptoss(self.deptos)
+		self.deptos = self.add_deptos_libres(self.deptos)
 		return r
 
 	#Calcula el total de Host segundo los requerimientos dados
@@ -71,8 +90,12 @@ class VLSM(object):
 		elif tipo_ip is 'B':
 			ultimo_bc_1 = '00000000'
 			ultimo_bc_2 = '00000000'
+
 			for d in self.deptos:
 				bits_host = d['bits_host']
+
+				if bits_host < 2:
+					continue
 
 				if bits_host > 8:
 					bits_octeto1 = bits_host - 8
@@ -137,43 +160,41 @@ class VLSM(object):
 	def setIP(self,ip):
 		self.ip = ip
 
-	def calculaLibres(self,bitsL):
-		clase = 'C'
+	def calculaLibres(self,bitsL,rangoBits):
+		libres = [bitsL]
+		rlibres = []
+		r = range(0,rangoBits)
 
-		if clase is 'C':
-			libres = [bitsL]
-			rlibres = []
-			r = range(0,8)
-			for l in libres:
-				prev = 255
-				flag = False
-				for p in r:
-					this = pow(2,p)
+		for l in libres:
+			prev = 255
+			flag = False
+			for p in r:
+				this = pow(2,p)
 
-					if this == l:
-						bits = this
-						pbits = p
-						flag = True
-						break
-					if this > l:
-						flag = False
-						break
-					
-					prev = this
+				if this == l:
+					bits = this
 					pbits = p
+					flag = True
+					break
+				if this > l:
+					flag = False
+					break
+				
+				prev = this
+				pbits = p
 
-				if flag:
-					rlibres.append((bits,pbits))
-					del libres[0]
-					print libres
-				else:
-					del libres[0]
-					
-					rlibres.append((prev,pbits))
-					libres.append(0)
-					libres.append((l - prev))
+			if flag:
+				rlibres.append((bits,pbits))
+				del libres[0]
+			else:
+				del libres[0]
+				
+				rlibres.append((prev,pbits))
+				libres.append(0)
+				libres.append((l - prev))
 
-			return rlibres
+		#print rlibres
+		return rlibres
 
 	#Imprime la 'tabla'
 	def imprimir(self):
